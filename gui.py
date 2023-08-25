739585a519554c5ca4b09cb6fd46c72788ead0d7
i5-PC <mcienkowski@gmail.com>
2023-08-25 13:28:38 +0200

    Implement label, user_input etc.

--- gui.py
+++ gui.py
@@ -1,2 +1,6 @@
 import PySimpleGui as sg
 
+label = sg.Text("Login App")
+user_input = sg.Input(key="user")
+password_input = sg.Input(key="password")
+login_button = sg.Button("Login", key="login")
