a3bf455b7f952a756c9854d666e500e7431a7823
i5-PC <mcienkowski@gmail.com>
2023-08-25 13:55:43 +0200

    Implement login and exit button

--- gui.py
+++ gui.py
@@ -6,13 +6,18 @@ user_input = sg.Input(key="user")
 pass_info = sg.Text("Enter password:")
 password_input = sg.Input(key="password")
 login_button = sg.Button("Login", key="login")
+exit_button = sg.Button("Exit", key="Exit")
 
-layout = [[label], [user_info, user_input], [pass_info, password_input]]
+layout = [[label], [user_info, user_input], [pass_info, password_input], [login_button, exit_button]]
 
 window = sg.Window("Login app", layout=layout, element_justification='c')
 
 while True:
-    key, value = window.read()
+    event, value = window.read()
+    match event:
 
+
+        case sg.WIN_CLOSED:
+            break
 
 window.close()
