0aecc89a7c22d4317c23b0af98987bf0e2c43917
i5-PC <mcienkowski@gmail.com>
2023-08-25 14:13:21 +0200

    Added dictionary to hold user login and password.

--- gui.py
+++ gui.py
@@ -6,7 +6,7 @@ user_input = sg.Input(key="user")
 pass_info = sg.Text("Enter password:")
 password_input = sg.Input(key="password")
 login_button = sg.Button("Login", key="login")
-exit_button = sg.Button("Exit", key="Exit")
+exit_button = sg.Button("Exit", key="exit")
 
 layout = [[label], [user_info, user_input], [pass_info, password_input], [login_button, exit_button]]
 
@@ -14,9 +14,16 @@ window = sg.Window("Login app", layout=layout, element_justification='c')
 
 while True:
     event, value = window.read()
+    dict_users = {}
     match event:
+        case "login":
+            user = value['user']
+            password = value['password']
+            dict_users[user] = password
+            print(dict_users)
 
-
+        case "exit":
+            break
         case sg.WIN_CLOSED:
             break
 
