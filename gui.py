a289fca589969e90d27e9e4bc68a48ba8d5dc1cb
i5-PC <mcienkowski@gmail.com>
2023-08-25 13:46:29 +0200

    Implement root window.

--- gui.py
+++ gui.py
@@ -1,6 +1,18 @@
-import PySimpleGui as sg
+import PySimpleGUI as sg
 
-label = sg.Text("Login App")
+label = sg.Text("Enter your login and password")
+user_info = sg.Text("Enter login:       ")
 user_input = sg.Input(key="user")
+pass_info = sg.Text("Enter password:")
 password_input = sg.Input(key="password")
 login_button = sg.Button("Login", key="login")
+
+layout = [[label], [user_info, user_input], [pass_info, password_input]]
+
+window = sg.Window("Login app", layout=layout, element_justification='c')
+
+while True:
+    key, value = window.read()
+
+
+window.close()
